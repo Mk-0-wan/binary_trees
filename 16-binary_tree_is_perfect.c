ad3f5fa9390eca0a2f5dafacae306851c2e0bf68
mk <mk@mail.com>
2024-02-02 02:48:53 +0300

    added custom function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,4 +1,15 @@
-#include "binary_trees.h"
+#include "binary_trees.h"  // Assuming you have the necessary header file
+
+/**
+ * depth_from_lsubtree - function that checks for the depth of the left
+ * subtree
+ * @root: pointer to the root node of the binary tree
+ * Return: the depth or sometimes called the max depth of the left subtree
+ */
+int depth_from_lsubtree(const binary_tree_t *root)
+{
+	return (root ? 1 + depth_from_lsubtree(root->left) : 0);
+}
 
 /**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect
@@ -7,14 +18,25 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int ht = 0, nodes = 0;
+	return (depth_match(tree, depth_from_lsubtree));
+}
 
-	if (!tree)
-		return (0);
+/**
+ * depth_match - function that checks if the lsubtree depth is the
+ * same as the rsubtree depth
+ * @root: pointer to the root node in the tree
+ * @depth: a function pointer to calculate the depth of a tree
+ * Return: 1 if both depths match, 0 otherwise
+ */
+int depth_match(const binary_tree_t *root, int (*depth)(const binary_tree_t *))
+{
+	if (!root)
+		return (1);
 
-	ht = binary_tree_height(tree);
-	nodes = binary_tree_size(tree);
+	int left_depth = depth(root->left);
+	int right_depth = depth(root->right);
 
-	/* No.nodes = 2^h + 1 */
-	return (pow(2, ht + 1) - 1 == nodes);
+	return ((left_depth == right_depth) &&
+		depth_match(root->left, depth) &&
+		depth_match(root->right, depth));
 }
