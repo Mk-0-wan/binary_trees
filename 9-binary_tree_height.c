e6ad386485a0d23ed4f0893c8875d515a8990736
mk <mk@mail.com>
2024-02-01 17:29:05 +0300

    final result

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,7 +6,7 @@
  * @root: pointer of the first node in the tree
  * Return: height of the lsubtree, otherwise 0 if fails
  */
-size_t height_of_rsubtree(binary_tree_t *root)
+size_t height_of_rsubtree(const binary_tree_t *root)
 {
 	size_t l = 0, r = 0;
 
@@ -23,7 +23,7 @@ size_t height_of_rsubtree(binary_tree_t *root)
  * @root: pointer of the first node in the tree
  * Return: height of the lsubtree, otherwise 0 if fails
  */
-size_t height_of_lsubtree(binary_tree_t *root)
+size_t height_of_lsubtree(const binary_tree_t *root)
 {
 	size_t l = 0, r = 0;
 
