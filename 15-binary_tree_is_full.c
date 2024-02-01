cd8c9d59f8065a2eed59fcbc3ab966ec51883ec2
mk <mk@mail.com>
2024-02-01 19:17:05 +0300

    looking into the children

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,42 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * h_rsubtree - function that calculates the height of the right
- * subtree
- * @root: pointer of the first node in the tree
- * Return: height of the lsubtree, otherwise 0 if fails
- */
-size_t h_rsubtree(const binary_tree_t *root)
-{
-	size_t l = 0, r = 0;
-
-	if (root == NULL)
-		return (0);
-
-	l = h_rsubtree(root->left);
-	r = h_rsubtree(root->right);
-
-	return (1 + max(l, r));
-}
-/**
- * h_lsubtree - function that calculates the height of the left subtree
- * @root: pointer of the first node in the tree
- * Return: height of the lsubtree, otherwise 0 if fails
- */
-size_t h_lsubtree(const binary_tree_t *root)
-{
-	size_t l = 0, r = 0;
-
-	if (root == NULL)
-		return (0);
-
-	l = h_lsubtree(root->left);
-	r = h_lsubtree(root->right);
-
-	return (1 + max(l, r));
-}
-
-/**
  * binary_tree_is_full - checks if binary_tree is full
  * @tree: pointer to the root node of the tree
  * Return: 1 true, 0 if tree is NULL
@@ -48,11 +12,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!tree->parent)
-		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
 
-	lsubtree = h_lsubtree(tree->left);
-	rsubtree = h_rsubtree(tree->right);
+	lsubtree = binary_tree_is_full(tree->left) ? 1 : 0;
+	rsubtree = binary_tree_is_full(tree->right) ? 1 : 0;
 
-	return ((lsubtree == rsubtree) ? 1 : 0);
+	return ((lsubtree && rsubtree));
 }
