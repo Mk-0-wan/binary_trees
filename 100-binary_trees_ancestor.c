6bcdca333c1c964de1ecd41cabca491a0cc7bfb8
mk <mk@mail.com>
2024-02-02 05:35:54 +0300

    finding the ancestors

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -69,8 +69,12 @@ binary_tree_t *find_ancestor_recursive(
 binary_tree_t *binary_trees_ancestor(
 		const binary_tree_t *first, const binary_tree_t *second)
 {
+	binary_tree_t *ancestor = NULL;
+
 	if (!first || !second)
 		return (NULL);
 
-	return (find_ancestor_recursive(first, second));
+	ancestor = find_ancestor_recursive(first, second);
+
+	return (ancestor);
 }
