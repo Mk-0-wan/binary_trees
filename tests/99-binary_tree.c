65d873db9aca953d4a4c4b8b140fd78e3a064bf2
mk <mk@mail.com>
2024-02-03 02:43:43 +0300

    pushed all the test cases for all the source files

---
+++ tests/99-binary_tree.c
@@ -0,0 +1,53 @@
+#include "binary_trees.h"
+/**
+ * height_of_rsubtree - function that calculates the height of the right
+ * subtree
+ * @root: pointer of the first node in the tree
+ * Return: height of the lsubtree, otherwise 0 if fails
+ */
+size_t height_of_rsubtree(binary_tree_t *root)
+{
+	size_t l = 0, r = 0;
+
+	if (root == NULL)
+		return (0);
+
+	l = height_of_rsubtree(root->left);
+	r = height_of_rsubtree(root->right);
+
+	return (1 + fmax(l, r));
+}
+/**
+ * height_of_lsubtree - function that calculates the height of the left subtree
+ * @root: pointer of the first node in the tree
+ * Return: height of the lsubtree, otherwise 0 if fails
+ */
+size_t height_of_lsubtree(binary_tree_t *root)
+{
+	size_t l = 0, r = 0;
+
+	if (root == NULL)
+		return (0);
+
+	l = height_of_lsubtree(root->left);
+	r = height_of_lsubtree(root->right);
+
+	return (1 + fmax(l, r));
+}
+/**
+ * binary_tree_height - measures the height of the binary_trees
+ * @tree: pointer to the root node of the tree
+ * Return: height of the node
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t lsubtree = 0, rsubtree = 0;
+
+	if (!tree)
+		return (0);
+
+	lsubtree = height_of_lsubtree(tree->left);
+	rsubtree = height_of_rsubtree(tree->right);
+
+	return (fmax(lsubtree, rsubtree));
+}
