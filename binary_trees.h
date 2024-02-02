d8cb1734daeee86aa1ab5679f93c9740796c52c3
mk <mk@mail.com>
2024-02-02 05:51:56 +0300

    updated

--- binary_trees.h
+++ binary_trees.h
@@ -55,4 +55,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 binary_tree_t *binary_trees_ancestor(
 		const binary_tree_t *first, const binary_tree_t *second);
+int binary_tree_is_complete(const binary_tree_t *tree);
 #endif
