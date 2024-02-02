e738eeafa1879cbbd637a22042de41c91586faa9
mk <mk@mail.com>
2024-02-02 03:23:25 +0300

    declaration of all the function inside the file

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,9 @@
 #include "binary_trees.h"
 
+int depth_match(const binary_tree_t *root,
+		int (*depth)(const binary_tree_t *));
+int depth_from_lsubtree(const binary_tree_t *root);
+
 /**
  * depth_from_lsubtree - function that checks for the depth of the left
  * subtree
@@ -9,18 +13,6 @@
 int depth_from_lsubtree(const binary_tree_t *root)
 {
 	return (root ? 1 + depth_from_lsubtree(root->left) : 0);
-}
-
-/**
- * binary_tree_is_perfect - function that checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree
- * Return: 1 if true, 0 if false
- */
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	if (!tree)
-		return (0);
-	return (depth_match(tree, depth_from_lsubtree));
 }
 
 /**
@@ -41,4 +33,16 @@ int depth_match(const binary_tree_t *root, int (*depth)(const binary_tree_t *))
 	return ((left_depth == right_depth) &&
 		depth_match(root->left, depth) &&
 		depth_match(root->right, depth));
+}
+
+/**
+ * binary_tree_is_perfect - function that checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree
+ * Return: 1 if true, 0 if false
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (depth_match(tree, depth_from_lsubtree));
 }
