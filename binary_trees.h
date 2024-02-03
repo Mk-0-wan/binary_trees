3d054e6593ec829b1ce719c2d13772c053d3a77a
mk <mk@mail.com>
2024-02-03 04:27:38 +0300

    s

--- binary_trees.h
+++ binary_trees.h
@@ -24,6 +24,7 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
+typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s binary_tree_t;
 
 /* Dependent fucntions */
@@ -61,5 +62,6 @@ binary_tree_t *binary_trees_ancestor(
 		const binary_tree_t *first, const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
 
 #endif
